8bd3036948450707ad52c899fb33f0934863fa34
Tauriq Hendricks <tauriqh786@gmail.com>
2020-08-07 16:18:52 +0200

    Added utility.ts

---
+++ app/utility.ts
@@ -0,0 +1,46 @@
+export class Utility {
+    static getInputValue(elementId: string): string {
+        const element: HTMLInputElement = <HTMLInputElement> document.getElementById(elementId)!;
+        return element.value;
+    }
+    
+    static titleCaseWord(word: string) {
+        if (!word) {
+            return word;
+        }
+        else {
+            return word[0].toUpperCase() + word.substr(1).toLowerCase();
+        }
+    }
+    
+    static logger(message: string) {
+        console.log(message);
+        console.error(message);
+    }
+
+    static isEmptyOrSpaces(str: string): boolean {
+        if (/^ *$/.test(str)) {
+            // It has only spaces, or is empty
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // get the new string without the empty values
+    static getStringWithoutSpaces(str: string): string {
+        let indexOfFirstLetter: number = 0;
+
+        for (let i = 0; i < str.length; i++) {
+            const character = str.charAt(i);
+            if (character !== ' ') {
+                indexOfFirstLetter = i;
+              break
+            }
+        }
+    
+        str = Utility.titleCaseWord(str.substring(indexOfFirstLetter));
+        return str;
+    }
+}
